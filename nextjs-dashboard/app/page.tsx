4a5c386925feeab8540966cd6f2abbbdf6830423
Emile Destrieux <emile.destrieux@systrangroup.com>
2024-05-25 11:23:13 +0200

    tuto nextjs-dashborad chap4 layout

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <h1 className="text-blue-500">I'm blue!</h1>
+      <h1 className="text-blue-500">I&apos;m blue!</h1>
       <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
 
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
